17f7202685fb500ccf3f082441c375cecd17323f
ihusamali <husamali6300@gmail.com>
2023-07-12 12:18:49 +0500

    Initial commit

---
+++ app/src/main/java/com/example/googleadmobinterstitialads/MainActivity.kt
@@ -0,0 +1,84 @@
+package com.example.googleadmobinterstitialads
+
+import android.content.Intent
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.util.Log
+import android.widget.Button
+import com.google.android.gms.ads.AdError
+import com.google.android.gms.ads.AdRequest
+import com.google.android.gms.ads.FullScreenContentCallback
+import com.google.android.gms.ads.LoadAdError
+import com.google.android.gms.ads.interstitial.InterstitialAd
+import com.google.android.gms.ads.interstitial.InterstitialAdLoadCallback
+
+class MainActivity : AppCompatActivity() {
+
+    private var mInterstitialAd: InterstitialAd? = null
+    private final var TAG = "MainActivity"
+    lateinit var btn : Button
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        btn = findViewById(R.id.button)
+
+        loadInterstitialAd()
+
+        btn. setOnClickListener {
+            showAd()
+        }
+    }
+
+    private fun showAd() {
+        if (mInterstitialAd != null){
+            mInterstitialAd?.fullScreenContentCallback = object : FullScreenContentCallback() {
+                override fun onAdClicked() {
+                    super.onAdClicked()
+                }
+
+                override fun onAdDismissedFullScreenContent() {
+                    super.onAdDismissedFullScreenContent()
+
+                    val intent  = Intent(this@MainActivity, MainActivity2::class.java)
+                    startActivity(intent)
+                }
+
+                override fun onAdFailedToShowFullScreenContent(p0: AdError) {
+                    super.onAdFailedToShowFullScreenContent(p0)
+                }
+
+                override fun onAdImpression() {
+                    super.onAdImpression()
+                }
+
+                override fun onAdShowedFullScreenContent() {
+                    super.onAdShowedFullScreenContent()
+                }
+            }
+
+            mInterstitialAd?.show(this)
+        }
+        else{
+            val intent  = Intent(this, MainActivity2::class.java)
+            startActivity(intent)
+        }
+    }
+
+    private fun loadInterstitialAd() {
+        var adRequest = AdRequest.Builder().build()
+
+        InterstitialAd.load(this,"ca-app-pub-3940256099942544/1033173712", adRequest, object : InterstitialAdLoadCallback() {
+            override fun onAdFailedToLoad(adError: LoadAdError) {
+                adError.toString().let { Log.d(TAG, it) }
+                mInterstitialAd = null
+            }
+
+            override fun onAdLoaded(interstitialAd: InterstitialAd) {
+                Log.d(TAG, "Ad was loaded.")
+                mInterstitialAd = interstitialAd
+            }
+        })
+    }
+}
